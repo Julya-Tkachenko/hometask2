0d04e021cdb781921e08b3ff09781d7b76d2184a
Julya-Tkachenko <julyagroma@gmail.com>
2024-12-08 13:01:43 +0200

    added new file hometask2

---
+++ index.js
@@ -0,0 +1,50 @@
+// Перелік типів данних
+
+// number
+
+let myNumber = 50;
+console.log(myNumber);
+myNumber = 100;
+console.log(myNumber);
+console.log(typeof myNumber);
+console.log("рядок" / 5);
+console.log(NaN + 1);
+console.log(NaN * 3);
+console.log(NaN ** 0);
+
+// string
+
+const myName = "Juliia";
+console.log(myName);
+console.log(typeof myName);
+
+//boolean
+
+const nameFieldChecked = true;
+console.log(nameFieldChecked);
+console.log(typeof nameFieldChecked);
+
+const ageFieldChecked = false;
+console.log(ageFieldChecked);
+console.log(typeof ageFieldChecked);
+
+const greater = 4 > 5;
+console.log(greater);
+
+//null
+const myAge = null;
+console.log(myAge);
+console.log(typeof myAge);
+
+// undefind
+
+let age;
+console.log(age);
+console.log(typeof age);
+
+//bigint
+
+let bigInt= 1234567890000n;
+console.log(typeof bigInt);
+console.log(bigInt);
+
